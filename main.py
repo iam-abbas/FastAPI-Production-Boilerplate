bb86933ec3dce851ee9a288d461ca128f11af50b
iam-abbas <42001049+iam-abbas@users.noreply.github.com>
2023-02-05 01:47:32 +0530

    directory structure

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+
+from core.config import config
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app="app:app",
+        host=config.APP_HOST,
+        port=config.APP_PORT,
+        reload=True if config.ENV != "production" else False,
+        workers=1,
+    )
